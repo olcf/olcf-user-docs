db7c4d6bf24533668385faeb07e11eee0fad7c5d
Jack Morrison <morrisonjc@ornl.gov>
2019-10-04 09:20:05 -0400

    Adds easy GitHub editing.

--- _static/js/custom.js
+++ _static/js/custom.js
@@ -1,12 +1,20 @@
-/*
 $( document ).ready(function() {
-  $(".wy-breadcrumbs-aside a").text("Go to OLCF Main Site");
-  $(".wy-breadcrumbs-aside a").attr("href", "https://www.olcf.ornl.gov").attr("target","_blank");
-});
+  // Create link and text for navigation back to the OLCF home page
+  var olcf_link = document.createElement("a");
+  var olcf_text = document.createTextNode("OLCF Home Page");
+  olcf_link.appendChild(olcf_text);
+  olcf_link.setAttribute("href", "https://olcf.ornl.gov");
 
-$( document ).ready(function() {
-  var olcf_site = "OLCF Main Site";
-  $(".wy-breadcrumbs-aside").text($(".wy-breadcrumbs-aside a").text() + " | " + olcf_site);
-  $(".wy-breadcrumbs-aside a").attr("href", "https://www.olcf.ornl.gov").attr("target","_blank");
+  // Open OLCF home page in new tab when clicked
+  olcf_link.setAttribute("target","_blank");
+
+  var separator = document.createTextNode(" | ");
+
+  // These items are right-aligned in the RTD theme breadcrumbs
+  aside = document.querySelector("body > div > section > div > div > div:nth-child(1) > ul > li.wy-breadcrumbs-aside");
+
+  // Next to the default "Edit on GitHub", add a separator, then the OLCF link.
+  aside.appendChild(separator);
+  aside.appendChild(olcf_link);
+
 });
-*/
