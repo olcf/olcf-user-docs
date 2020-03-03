b76d17b1ca4b1237fe7891c2bb8849e9ca3d06aa
Jack Morrison <morrisonjc@ornl.gov>
2020-03-03 10:06:36 -0500

    Adds external link icon to main nav training section

--- _static/js/custom.js
+++ _static/js/custom.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+
   // Create link and text for navigation back to the OLCF home page
   var olcf_link = document.createElement("a");
   var olcf_text = document.createTextNode("OLCF Home Page");
@@ -17,7 +18,7 @@ $( document ).ready(function() {
   aside.appendChild(separator);
   aside.appendChild(olcf_link);
 
-// Insert Project Name "OLCF User Documentation" below html_logo in sidebar navigation
+  // Insert Project Name "OLCF User Documentation" below html_logo in sidebar navigation
   var project_name_link = document.createElement("a");
   var project_name_text = document.createTextNode(" OLCF User Documentation");
   project_name_link.appendChild(project_name_text);
@@ -26,4 +27,30 @@ $( document ).ready(function() {
   project_name_link.classList.add("icon-home");
   wysidenavsearch = document.querySelector("body > div > nav > div > div.wy-side-nav-search > a");
   wysidenavsearch.appendChild(project_name_link);
+
+
+  function iconize_external_links(nav_level){
+    child_list_items = nav_level.getElementsByTagName("LI");
+    var i, j;
+    for (i = 0; i < child_list_items.length; ++i){
+      a_elements = child_list_items[i].getElementsByTagName("A");
+      for (j = 0; j < a_elements.length; ++j){
+        a_element = a_elements[j];
+        if (a_element.getAttribute("href").includes("http")){
+          var icon = document.createElement("i");
+          icon.classList.add("fa");
+          icon.classList.add("fa-external-link");
+
+          var spacer = document.createTextNode(" ");
+          a_element.appendChild(spacer);
+          a_element.appendChild(icon);
+        }
+      }
+    }
+  }
+
+  main_nav = document.querySelector("body > div > nav > div > div.wy-menu.wy-menu-vertical");
+  training_nav = document.querySelector("body > div > nav > div > div.wy-menu.wy-menu-vertical > ul:nth-child(6) > li");
+  iconize_external_links(training_nav)
+
 });
