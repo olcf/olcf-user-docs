b2cba9cef20c253f1e999a21542400dbf915823a
Michael Sandoval <sandoval.michael510@gmail.com>
2025-07-18 10:26:53 -0400

    Theme: added myOLCF link to top banner

--- _static/js/custom.js
+++ _static/js/custom.js
@@ -9,14 +9,25 @@ $( document ).ready(function() {
   // Open OLCF home page in new tab when clicked
   olcf_link.setAttribute("target","_blank");
 
+  // Create link and text for navigation back to the myOLCF home page
+  var myolcf_link = document.createElement("a");
+  var myolcf_text = document.createTextNode("myOLCF");
+  myolcf_link.appendChild(myolcf_text);
+  myolcf_link.setAttribute("href", "https://my.olcf.ornl.gov");
+
+  // Open myOLCF home page in new tab when clicked
+  myolcf_link.setAttribute("target","_blank");
+
   var separator = document.createTextNode(" | ");
 
   // These items are right-aligned in the RTD theme breadcrumbs
   aside = document.querySelector("body > div > section > div > div > div:nth-child(1) > ul > li.wy-breadcrumbs-aside");
 
-  // Next to the default "Edit on GitHub", add a separator, then the OLCF link.
+  // Next to the default "Edit on GitHub", add a separator, then the OLCF links.
   aside.appendChild(separator);
   aside.appendChild(olcf_link);
+  aside.appendChild(separator.cloneNode(true)) // have to append a copy
+  aside.appendChild(myolcf_link);
 
   // Insert "Help email" below html_logo in sidebar navigation
   var help_link = document.createElement("a");
